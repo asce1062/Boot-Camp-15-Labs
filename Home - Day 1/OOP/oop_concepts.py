814ec6ce2f65e58508476187400c45caa269f747
Alex.Immer <tnkratos@gmail.com>
2017-02-08 17:58:30 +0300

    Create a student instance

--- Home - Day 1/OOP/oop_concepts.py
+++ Home - Day 1/OOP/oop_concepts.py
@@ -1,7 +1,9 @@
 # real-world problem modeled using OOP
 # Depicts inheritance, encapsulation, polymorphism and the other OOP concepts.
 
+
 class Person:
+
     def __init__(self, name, surname, id_number, student_type):
         self.name = name
         self.surname = surname
@@ -14,7 +16,6 @@ class Student(Person):
     def __init__(self, student_type, *args, **kwargs):
         self.student_type = student_type
         self.classes = []
-        super(Student, self).__init__(*args, **kwargs)
 
     def enrol(self, course):
         self.classes.append(course)
@@ -25,15 +26,5 @@ class Student(Person):
     def assign_teaching(self, course):
         self.courses_taught.append(course)
 
-    def menu(self, name):
-        print("Welcome ",self.name, "! Kindly select an option:")
-        print ("""1: Enroll
-                  2: Courses taught
-                  3: Lecturer assigned""")
-        option = input("Option: ")
-        if option == "1":
-            return enrol()
-        elif option == "2":
-            return courses_taught()
-        elif option == "3":
-            return assign_teaching()
+Alex = Student(Student.POSTGRADUATE, "Alex", "Ngugi", "BIT-035-145/2012")
+Alex.enrol(a_postgrad_course)
