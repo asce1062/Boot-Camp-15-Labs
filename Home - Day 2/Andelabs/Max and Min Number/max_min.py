c35a17a6e2b370571596b7edc91a77e5bf1b4b13
Alex.Immer <tnkratos@gmail.com>
2017-02-08 13:27:59 +0300

    max_min solution

---
+++ Home - Day 2/Andelabs/Max and Min Number/max_min.py
@@ -0,0 +1,10 @@
+def find_max_min(numbers):
+    num_list = []
+    minimum = min(numbers)
+    maximum = max(numbers)
+    if maximum == minimum:
+        num_list.append(len(numbers))
+    else:
+        num_list.append(minimum)
+        num_list.append(maximum)
+    return num_list
